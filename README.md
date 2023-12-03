ccce6095467b465038841ca0fe466716f9be0230
SaraswathiSP <spsaraswathi2029@gmail.com>
2023-12-03 14:05:06 +0530

    changes

---
+++ README.md
@@ -0,0 +1,35 @@
+# Travelopia Assignment
+# Overview
+This is a simple  Responsive design for various screen sizes (desktop, tablet,mobile).It contains dynamic content toggle for tour details with "Read More/Read Less" functionality,Mobile navigation for easy access on smaller screens.
+
+
+# Features
+
+1) Responsive design for various screen sizes (desktop, tablet,mobile).
+
+2) Dynamic content toggle for tour details with "Read More/Read Less" functionality.
+
+3) Mobile navigation for easy access on smaller screens.
+
+4) Header with trust and guarantee icons.
+
+5) Hero section for highlighting tour details.
+
+6) Footer with important links and information.
+
+
+# Technologies Used
+
+. JavaScript
+
+. HTML
+
+. CSS
+
+
+## Deployment
+
+To deploy this project run
+
+
+Deployed Netlify Link : ""
--- script.js
+++ script.js
@@ -1,25 +1,27 @@
 
 
-// const mobileNavContainer = document.getElementById('mobileNavContainer');
-// const mobileNavIcon = document.getElementById('mobileNavIcon');
-// const mobileNavCloseIcon = document.getElementById('mobileNavCloseIcon');
+const mobileNavContainer = document.getElementById('mobileNavContainer');
+const mobileNavIcon = document.getElementById('mobileNavIcon');
+const mobileNavCloseIcon = document.getElementById('mobileNavCloseIcon');
 
-// mobileNavIcon.addEventListener('click', function () {
-//     mobileNavContainer.classList.add('show');
-//     mobileNavIcon.style.display = 'none';
-//     mobileNavCloseIcon.style.display = 'flex';
-// });
+if (mobileNavIcon) {
+  mobileNavIcon.addEventListener("click", function () {
+    const isOpen = mobileNavContainer.style.display !== "block";
 
-// mobileNavCloseIcon.addEventListener('click', function () {
-//     mobileNavContainer.classList.remove('show');
-//     mobileNavIcon.style.display = 'flex';
-//     mobileNavCloseIcon.style.display = 'none';
-// });
+    if (isOpen) {
+      mobileNavContainer.style.display = "block";
+      mobileNavIcon.innerHTML = '<i class="fas fa-times"></i>';
+    } else {
+      mobileNavContainer.style.display = "none";
+      mobileNavIcon.innerHTML = '<i class="fas fa-bars"></i>'
 
+    }
+  });
+}
 
 
 const shortText = "Armenia is a mountainous nation in West Asia, A former Soviet Republic, and was one of the first Christian nations, embracing Christianity in the fourth century. This rich cultural and natural... Environment makes it an exciting destination for..."
-const completeText = "Armenia is a mountainous nation in West Asia, A former Soviet Republic, and was one of the first Christian nations, embracing Christianity in the fourth century. This rich cultural and natural environment makes it an exciting destination for a wide range of adventure, activity and walking holidays. There are many pagan sites to visit as well as UNESCO World Heritage-listed churches, which are -- almost without exception -- built in stunning locations. Surrounded by forests, perched on the top of dramatic gorges or silhouetted before snow-capped mountains, they draw us to magnificent open spaces filled with a huge variety of trees and wild flowers. On Armenia tours we see wonderful mountains lakes and waterfalls in the tour."
+const completeText = "Armenia is a mountainous nation in West Asia, A former Soviet Republic, and was one of the first Christian nations, embracing Christianity in the fourth century. This rich cultural and natural environment makes it an exciting destination for a wide range of adventure, activity and walking holidays. <br/> <br/> There are many pagan sites to visit as well as UNESCO World Heritage-listed churches, which are -- almost without exception -- built in stunning locations. Surrounded by forests, perched on the top of dramatic gorges or silhouetted before snow-capped mountains, they draw us to magnificent open spaces filled with a huge variety of trees and wild flowers. On Armenia tours we see wonderful mountains lakes and waterfalls in the tour."
 
 const tourDetails = document.getElementById("tourDetails")
 const buttonElement = document.getElementById("toggleButton")
@@ -27,10 +29,10 @@ const buttonElement = document.getElementById("toggleButton")
 buttonElement.addEventListener('click', () => {
     if (buttonElement.textContent === "Read More") {
         buttonElement.textContent = "Read Less";
-        tourDetails.textContent = completeText;
+        tourDetails.innerHTML = completeText;
     } else {
         buttonElement.textContent = "Read More";
-        tourDetails.textContent = shortText
+        tourDetails.innerHTML = shortText
     }
 
 })
